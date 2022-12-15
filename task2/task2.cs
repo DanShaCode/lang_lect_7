c176132f444fdeb60e73c796d90de6c26c2fd70c
Danis Sharipov <danshadance@mail.ru>
2022-12-15 23:37:13 +0600

    голопом по Европам

---
+++ task2/task2.cs
@@ -0,0 +1,16 @@
+﻿// Сумма числе от 1 до N
+
+int SumFor(int n) // Решение задачи через Цикл
+{
+ int result = 0;
+ for (int i = 1; i <= n; i++) result += i;
+ return result;
+}
+
+int SumRec(int n) // Решение задачи через рекурсию
+{
+ if (n == 0) return 0;
+ else return n + SumRec(n - 1);
+}
+Console.WriteLine(SumFor(10)); // 55
+Console.WriteLine(SumRec(10)); // 55
